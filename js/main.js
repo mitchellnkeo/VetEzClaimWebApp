6608178e5428aceb67f0bf92f093572389e0162e
Omar Melendrez <omar.melendrez@gmail.com>
2025-02-03 12:28:30 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  'use strict';
+  //===== Counter Up =====//
+  if ($.isFunction($.fn.counterUp)) {
+    $('.counter').counterUp({
+      delay: 10,
+      time: 2000
+    });
+  }
+});
