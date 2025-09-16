25c069b5753a9f9c297746fee3ecd72f2a727a9e
hagenssd <105477895+hagenssd@users.noreply.github.com>
2025-09-16 13:07:03 +0600

    feat (slice): Buddy slice added for app state management

---
+++ store/slices/buddySlice.js
@@ -0,0 +1,31 @@
+// buddyStatementThunks.js
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import ApiService from '@/utils/apiService';
+
+export const sendDocToWitness = createAsyncThunk(
+  "buddyStatement/sendDocToWitness",
+  async (values, thunkAPI) => {
+    console.log("Here values >> ", values);
+
+    return ApiService.post(`/buddy-statement/send-doc-to-wetness`, values, {
+        noAuth: true,
+      }).then((response) => {
+        console.log("Here response >> ", response);
+        return response.data;
+      });
+  }
+);
+
+export const sendMessageToWitness = createAsyncThunk(
+  "buddyStatement/sendMessageToWitness",
+  async (values, thunkAPI) => {
+    try {
+      const response = await ApiService.post('/buddy-statement/send-doc-to-wetness', values, { noAuth: true });
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(
+        error.response?.data || "Failed to send message to witness."
+      );
+    }
+  }
+);
