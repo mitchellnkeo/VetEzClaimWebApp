25c13977769f028e8b19f31d265cfc658e7a528d
hagenssd <105477895+hagenssd@users.noreply.github.com>
2025-09-24 19:35:12 +0600

    feat (Utils): necessary utils functions added for back pay calculator

--- utils/utils.js
+++ utils/utils.js
@@ -11,3 +11,20 @@ export const formatAmount = async (amount) => {
   }
   return amount;
 }
+
+export const IsValidDateRange = (initialMonth, initialYear, finalMonth, finalYear) => {
+  const monthToNumber = {
+    'January': 0, 'February': 1, 'March': 2, 'April': 3, 'May': 4, 'June': 5,
+    'July': 6, 'August': 7, 'September': 8, 'October': 9, 'November': 10, 'December': 11
+  };
+
+  if (!(initialMonth in monthToNumber) || !(finalMonth in monthToNumber)) {
+    throw new Error('Invalid month name provided.');
+  }
+
+  const initialDate = new Date(parseInt(initialYear), monthToNumber[initialMonth]);
+  const finalDate = new Date(parseInt(finalYear), monthToNumber[finalMonth]);
+
+  return finalDate >= initialDate;
+};
+
